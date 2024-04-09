01a027a7c14e9e94e0654544ae5563e9317cb0ba
Usman Malik <usmanmalik9797@gmail.com>
2024-04-09 17:44:13 -0500

    Implemented CRUD using MediatR pattern

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -1,9 +1,6 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
@@ -20,6 +17,25 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActvity(Guid id){
             
             return await Mediator.Send(new Details.Query{Id=id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity){
+            await  Mediator.Send(new Create.Command{Activity = activity});
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async  Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity){
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command{Activity = activity});
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id){
+            await Mediator.Send(new Delete.Command{Id = id});
+            return Ok();
         }
     }
 }
--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -1,32 +1,11 @@
-using Application.Activities;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors(opt =>{
-    opt.AddPolicy("CorsPolicy" , policy =>{
-        policy.AllowAnyHeader()
-        .AllowAnyMethod()
-        .WithOrigins("http://localhost:3000");
-    });
-});
-
-builder.Services.AddMediatR
-                (cfg => cfg.RegisterServicesFromAssemblies
-                (typeof(List.Handler).Assembly));
-
-                
+builder.Services.AddApplicationServices(builder.Configuration);            
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
